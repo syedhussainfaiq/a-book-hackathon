efe0a9bc657840d14c34051768a3e74f9925b16f
SYED FAIQ HUSSAIN <159991416+syedhussainfaiq@users.noreply.github.com>
2025-12-23 23:27:43 +0500

    inital commit

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -85,7 +85,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Modules',
+          label: 'Text Book',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
